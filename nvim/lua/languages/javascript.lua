fe92931c87df149250000eed33264e48b94f55a3
YukitoTao <yukitoTao@gmail.com>
2021-10-09 17:11:59 +0700

    [NeoVim] Add Javascript, HTML, CSS, TypeScript language server protocol

--- nvim/lua/languages/javascript.lua
+++ nvim/lua/languages/javascript.lua
@@ -1,4 +1,15 @@
 require('notify')(
-  "Please install following LSP to enable this feature.\nRun:",
+  "Please install following LSP to enable this feature.\nRun: npm i -g vscode-langservers-extracted stylelint-lsp typescript typescript-language-server",
   "warn"
 )
+
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require('lspconfig').html.setup {
+  capabilities = capabilities,
+}
+require('lspconfig').stylelint_lsp.setup{}
+require('lspconfig').tsserver.setup{}
+
